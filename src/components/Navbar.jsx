b25211ef7da7ab113d6f285485af97f462c79b89
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-18 00:01:30 +0530

    making changes to links routing each page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
-import { logo } from "../data/data.json";
+import logo from "../data/data.json";
+
+const searchIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 0 24 24"
+    width="24px"
+    fill="#000000"
+  >
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+  </svg>
+);
 const Navbar = () => {
   return (
     <div className="nav">
@@ -10,30 +23,34 @@ const Navbar = () => {
           <img src={logo} alt="Not" id="logoImage" />
         </Link>
 
-        <a className="navlinks" href="/#miphones">
+        <Link className="navlinks" to="/miphones">
           Mi Phones
-        </a>
-        <a className="navlinks" href="/#redmiphones">
+        </Link>
+        <Link className="navlinks" to="/redmiphones">
           Redmi Phones
-        </a>
-        <a className="navlinks" href="/#tv">
+        </Link>
+        <Link className="navlinks" to="/tv">
           TV
-        </a>
-        <a className="navlinks" href="/#laptops">
+        </Link>
+        <Link className="navlinks" to="/laptops">
           Laptops
-        </a>
-        <a className="navlinks" href="/#lifestyle">
+        </Link>
+        <Link className="navlinks" to="/lifestyle">
           Fitness & Lifestyle
-        </a>
-        <a className="navlinks" href="/#home">
+        </Link>
+        <Link className="navlinks" to="/home">
           Home
-        </a>
-        <a className="navlinks" href="/#audio">
+        </Link>
+        <Link className="navlinks" to="/audio">
           Radio
-        </a>
-        <a className="navlinks" href="/#accessories">
+        </Link>
+        <Link className="navlinks" to="/accessories">
           Accessories
-        </a>
+        </Link>
+        <div className="searchbox">
+          <input type="text" name="search" placeholder="Search Producs" />
+          {searchIcon}
+        </div>
       </div>
     </div>
   );
