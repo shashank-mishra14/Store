8f28a3e18e2b8714dc2adc89903711d1c08fa57c
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-18 20:59:58 +0530

    making offers cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Slider from "./components/Slider.jsx";
-import {banner} from "./data/data.json";
+import data from "./data/data.json";
+import Offers from "./components/Offers.jsx";
 function App() {
   return (
     <div>
       <Router>
         <PreNavbar />
         <Navbar />
-        <Slider start={banner}/>
+        <Slider start={data.banner.start}/>
+        <Offers  offers={data.offer}/>
       </Router>
     </div>
   );
