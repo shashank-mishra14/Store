1c64e0a5824a72dc69c86a8c37fd1ef2ea2d3a58
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-18 20:36:09 +0530

    importing banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import Slider from "./components/Slider.jsx";
+import {banner} from "./data/data.json";
 function App() {
   return (
     <div>
       <Router>
         <PreNavbar />
         <Navbar />
-        <Slider />
+        <Slider start={banner}/>
       </Router>
     </div>
   );
