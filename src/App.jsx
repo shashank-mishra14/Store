cb3352593c5429d73e32a2c27438b29f63859d1b
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-18 20:29:41 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import PreNavbar from "./components/PreNavbar";
 import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import Slider from "./components/Slider.jsx";
 function App() {
   return (
     <div>
       <Router>
         <PreNavbar />
         <Navbar />
+        <Slider />
       </Router>
     </div>
   );
