71e27e1e097d0a8bf0d3b78678dbadcc80e3a443
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-19 22:13:10 +0530

    updating navbar a tags

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import "../styles/Navbar.css";
-import { Link } from "react-router-dom";
-import logo from "../data/data.json";
+import "../styles/nav.css";
+import { logo } from "../data/data.json";
 
 const searchIcon = (
   <svg
@@ -15,43 +14,46 @@ const searchIcon = (
     <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
   </svg>
 );
+
 const Navbar = () => {
   return (
     <div className="nav">
       <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Not" id="logoImage" />
-        </Link>
-        </div>
-        <Link className="navlinks" to="/miphones">
-          Mi Phones
-        </Link>
-        <Link className="navlinks" to="/redmiphones">
-          Redmi Phones
-        </Link>
-        <Link className="navlinks" to="/tv">
-          TV
-        </Link>
-        <Link className="navlinks" to="/laptops">
-          Laptops
-        </Link>
-        <Link className="navlinks" to="/lifestyle">
-          Fitness & Lifestyle
-        </Link>
-        <Link className="navlinks" to="/home">
-          Home
-        </Link>
-        <Link className="navlinks" to="/audio">
-          Radio
-        </Link>
-        <Link className="navlinks" to="/accessories">
-          Accessories
-        </Link>
-        <div className="searchbox">
-          <input type="text" name="search" placeholder="Search Producs" />
-          {searchIcon}
-        </div>
-      </div>        
+        <a href="/">
+          <img id="logoImage" src={logo} alt="Not" />
+        </a>
+      </div>
+
+      <a className="navlinks" href="/#miphones">
+        Mi Phones
+      </a>
+      <a className="navlinks" href="/#redmiphones">
+        Redmi Phones
+      </a>
+      <a className="navlinks" href="/#tv">
+        TV
+      </a>
+      <a className="navlinks" href="/#laptops">
+        Laptops
+      </a>
+      <a className="navlinks" href="/#lifestyle">
+        Fitness & Lifestyle
+      </a>
+      <a className="navlinks" href="/#home">
+        Home
+      </a>
+      <a className="navlinks" href="/#audio">
+        Radio
+      </a>
+      <a className="navlinks" href="/#accessories">
+        Accessories
+      </a>
+
+      <div className="searchbox">
+        <input type="text" name="search" placeholder="Search Products" />
+        {searchIcon}
+      </div>
+    </div>
   );
 };
 
