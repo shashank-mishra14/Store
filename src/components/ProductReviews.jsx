263075d4558dddc1943cf29b90bc19e7cdeab53e
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-19 15:58:04 +0530

    updated the ui of Product card and Review

--- src/components/ProductReviews.jsx
+++ src/components/ProductReviews.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../styles/ProductReviews.css'
-import ProductReviewCard from './components/ProductReviewCard.jsx'
+import ProductReviewCard from './ProductReviewCard.jsx'
 const ProductReviews = ({productReviews}) => {
   return (
     <div className='productReviews'>
         {productReviews.map((item, index)=>(
-            <ProductReviewCard price={item.price} name={item.name} review= {item.review} index= {index} key={item.image} />
+            <ProductReviewCard price={item.price} name={item.name} review= {item.review} index= {index} key={item.imagewfegw} />
         ))}
     </div>
   )
