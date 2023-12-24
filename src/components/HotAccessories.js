c84c97df9491cea8ca1a51e3324ed6cb569b5d4a
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-24 13:46:29 +0530

    major changes

--- src/components/HotAccessories.js
+++ src/components/HotAccessories.js
@@ -1,7 +1,8 @@
 import React from "react";
-import "../styles/HotAcceessories.css";
-import HotItemcard from "./HotItemcard.jsx";
-const HotAcceessories = (
+import HotItemCard from "./HotItemcard.js"; // Corrected the file name casing
+import "../styles/HotAccessories.css";
+
+const HotAccessories = ({
   music,
   musicCover,
   smartDevice,
@@ -11,73 +12,82 @@ const HotAcceessories = (
   lifeStyle,
   lifeStyleCover,
   mobileAccessories,
-  mobileAccessoriesCover
-) => {
+  mobileAccessoriesCover,
+}) => {
   return (
-    <div className="HotAcceessories">
+    <div className="HotAccessories">
       <div>
-        <img src={musicCover || smartDeviceCover || homeCover || lifeStyleCover || mobileAccessoriesCover} alt="Cover" />
+        <img
+          src={
+            musicCover ||
+            smartDeviceCover ||
+            homeCover ||
+            lifeStyleCover ||
+            mobileAccessoriesCover
+          }
+          alt="Cover"
+        />
       </div>
 
+      {/* -----------2nd */}
       <div>
         {music &&
           music.map((item, index) => (
-            <HotItemcard
-              key={item.img}
+            <HotItemCard
+              key={item.image}
               name={item.name}
               price={item.price}
               image={item.image}
               index={index}
             />
           ))}
-        ;
         {smartDevice &&
           smartDevice.map((item, index) => (
-            <HotItemcard
-              key={item.img}
+            <HotItemCard
+              key={item.image}
               name={item.name}
               price={item.price}
               image={item.image}
               index={index}
             />
           ))}
-        ;
         {home &&
           home.map((item, index) => (
-            <HotItemcard
-              key={item.img}
+            <HotItemCard
+              key={item.image}
               name={item.name}
               price={item.price}
               image={item.image}
               index={index}
             />
           ))}
-          ;
-          {lifeStyle &&
+
+        {lifeStyle &&
           lifeStyle.map((item, index) => (
-            <HotItemcard
-              key={item.img}
+            <HotItemCard
+              key={item.image}
               name={item.name}
               price={item.price}
               image={item.image}
               index={index}
             />
           ))}
-          ;
-          {mobileAccessories &&
+
+        {mobileAccessories &&
           mobileAccessories.map((item, index) => (
-            <HotItemcard
-              key={item.img}
+            <HotItemCard
+              key={item.image}
               name={item.name}
               price={item.price}
               image={item.image}
               index={index}
             />
           ))}
-          ;
+
+        <HotItemCard image="https://i01.appmifile.com/webfile/globalimg/in/cms/593181BE-7DB8-15F5-E907-506427B8C9F4.jpg" />
       </div>
     </div>
   );
 };
 
-export default HotAcceessories;
+export default HotAccessories;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/nav.css";
-import { logo } from "../data/data.json";
+import logo from "../data/data.json";
 
 const searchIcon = (
   <svg
